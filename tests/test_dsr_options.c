a4ef8dca351f6dc0a2ea76d16f20669c704fee20
Kamil Wcislo <mek.xgt@gmail.com>
2015-10-18 22:22:01 +0200

    tests: length variable - make common

--- tests/test_dsr_options.c
+++ tests/test_dsr_options.c
@@ -6,10 +6,12 @@
 TEST_GROUP(test_DSR_options);
 
 uint8_t message_buffer[10];
+int length;
 
 TEST_SETUP(test_DSR_options)
 {
     memset(message_buffer, 0xa5, sizeof(message_buffer));
+    length = 0;
 }
 
 TEST_TEAR_DOWN(test_DSR_options)
@@ -19,7 +21,6 @@ TEST_TEAR_DOWN(test_DSR_options)
 TEST(test_DSR_options, createRREQOption)
 {
     uint8_t expected_output[] = {0x2, 2 ,0, 0};
-    int length;
 
     length = createRREQMsg(message_buffer, sizeof(message_buffer), 0, 0);
 
@@ -30,7 +31,6 @@ TEST(test_DSR_options, createRREQOption)
 TEST(test_DSR_options, createRREQsWithDifferentTargetsAndIds)
 {
     uint8_t expected_output[] = {0x2, 2 ,1, 1};
-    int length;
 
     length = createRREQMsg(message_buffer, sizeof(message_buffer), 1, 1);
     TEST_ASSERT_EQUAL_MEMORY(expected_output, message_buffer, length);
@@ -43,8 +43,6 @@ TEST(test_DSR_options, createRREQsWithDifferentTargetsAndIds)
 
 TEST(test_DSR_options, createRREQMsgReturnsErrorWhenBufferTooSmall)
 {
-    int length;
-
     length = createRREQMsg(message_buffer, 2, 0, 0);
     TEST_ASSERT_EQUAL(-1, length);
 }
@@ -53,7 +51,6 @@ TEST(test_DSR_options, createRREPOption)
 {
     uint8_t expected_output[] = {0x3, 0x03, 0x01, 0x02, 0x03};
     uint8_t addr_list[] = {0x01, 0x02, 0x03};
-    int length;
 
     length = createRREPMsg(message_buffer, sizeof(message_buffer), addr_list, sizeof(addr_list));
     TEST_ASSERT_EQUAL(sizeof(expected_output), length);
@@ -62,8 +59,6 @@ TEST(test_DSR_options, createRREPOption)
 
 TEST(test_DSR_options, createRREPMsgReturnsErrorWhenBufferTooSmall)
 {
-    int length;
-
     length = createRREPMsg(message_buffer, 2, NULL, 1);
     TEST_ASSERT_EQUAL(-1, length);
 }
@@ -71,7 +66,6 @@ TEST(test_DSR_options, createRREPMsgReturnsErrorWhenBufferTooSmall)
 TEST(test_DSR_options, createRERROption)
 {
     uint8_t expected_output[] = {0x1, 0x3, 0x0, 0x1, 0x2};
-    int length;
 
     length = createRERRMsg(message_buffer, 2, 0, 0 ,0);
     TEST_ASSERT_EQUAL(-1, length);
@@ -92,7 +86,6 @@ TEST(test_DSR_options, createRERROption)
 TEST(test_DSR_options, createAREQOption)
 {
     uint8_t expected_output[] = {0x4, 0x1, 0x1};
-    int length;
 
     length = createAREQMsg(message_buffer, 2, 0);
     TEST_ASSERT_EQUAL(-1, length);
@@ -105,7 +98,6 @@ TEST(test_DSR_options, createAREQOption)
 TEST(test_DSR_options, createAREPOption)
 {
     uint8_t expected_output[] = {0x5, 0x3, 0x0, 0x1, 0x2};
-    int length;
 
     length = createAREPMsg(message_buffer, 2, 0, 1, 2);
     TEST_ASSERT_EQUAL(-1, length);
@@ -119,7 +111,6 @@ TEST(test_DSR_options, createROUTOption)
 {
     uint8_t expected_output[] = {0x6, 0x3, 0x2, 0x1, 0x2};
     uint8_t addr_list[] = {0x01, 0x02};
-    int length;
 
     length = createROUTMsg(message_buffer, 2, addr_list, 2);
     TEST_ASSERT_EQUAL(-1, length);
@@ -133,7 +124,6 @@ TEST(test_DSR_options, createDATAOption)
 {
     uint8_t expected_output[] = {0x7, 0x3, 0xaa, 0x55, 0xaa};
     uint8_t data[] = {0xaa, 0x55, 0xaa};
-    int length;
 
     length = createDATAMsg(message_buffer, 2, data, 3);
     TEST_ASSERT_EQUAL(-1, length);
